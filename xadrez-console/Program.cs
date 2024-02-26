2b0e6af3e6845979213fc150c689294fbec7c194
Gabriel Alves <gabriel.alves2301@hotmail.com>
2024-02-26 17:11:54 -0300

    Criada a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using xadrez_console.tabuleiro;
 
 namespace MyApp
 {
@@ -6,7 +7,11 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
+
+            Console.ReadLine();
         }
     }
 }
