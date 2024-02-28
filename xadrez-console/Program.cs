59c99a66a589a0e5b8dce83c5ebd92ad3ea4f57b
Gabriel Alves <gabriel.alves2301@hotmail.com>
2024-02-28 17:19:30 -0300

    Criando excetion personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,13 +8,20 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
 
-            Tela.ImprimirTabuleiro(tabuleiro);
+                Tela.ImprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException tab)
+            {
+                Console.WriteLine(tab.Message);
+            }
 
             Console.ReadLine();
         }
